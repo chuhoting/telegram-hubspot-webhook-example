6fdd6026bd3ef7078d9281baaae94dc07777663a
chuhoting <chu-ho-ting@outlook.com>
2019-02-12 19:26:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const request = require("request");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(express.static("public"));
 
 app.listen(process.env.PORT || 3000,function(req,res){
@@ -19,6 +19,13 @@ app.post("/hook",function(req,res){
   var message = objectfirstname+" "+objectlastname+" is enrolled into workflow";
 
   var options = {
-    url:"https://api.telegram.org/bot743501577:AAFeulDtnW13uGflLZn6DmHT-GwbSNPGjLw/sendMessage?text="+message+"&chat_id=@hstestchannelapac"
+    url:"https://api.telegram.org/BOT_ID:SECRET_KEY/sendMessage?text="+message+"&chat_id=@hstestchannelapac"
   };
+
+  request(options,function(error,response,body){
+
+  });
+  
+  res.status(200);
+  res.send();
 });
